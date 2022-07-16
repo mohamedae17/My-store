7dbe2981130c3ba5d4be853e0b96dfb5cbf1495f
Mohamed Afify <mohamed.ae1717@gmail.com>
2022-07-16 11:05:38 +0200

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { CartComponent } from './cart/cart.component';
+import { ProductListComponent } from './product-list/product-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/Prouctlist',pathMatch:'full'},
+  {path:'Prouctlist' , component:ProductListComponent},
+  {path:'cart' , component:CartComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
